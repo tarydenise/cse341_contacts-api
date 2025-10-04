262c2fd8307cc77255e962dab9c0060e80fe2c75
tarydenise <tarydenise@gmail.com>
2025-10-04 15:49:56 -0700

    Week 2 Team Stretch challenge

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const { WebsiteRoutes } = require("./routes");
 const cors = require("cors");
 const app = express();
@@ -6,6 +8,17 @@ const port = process.env.port || 8080;
 
 app.use(cors());
 app.use("/", WebsiteRoutes);
+
+console.log(process.env.MONGODB_URI)
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
