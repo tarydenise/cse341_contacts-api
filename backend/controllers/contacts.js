cd9562100b1d75470eaeaaade96ec50ca031bdf8
tarydenise <tarydenise@gmail.com>
2025-10-07 23:47:57 -0700

    Week 3 Requests without ESLint and Prettier

--- backend/controllers/contacts.js
+++ backend/controllers/contacts.js
@@ -1,42 +1,96 @@
 const mongodb = require("../db/connect");
-const { ObjectId } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
-const getAll = async (req, res, next) => {
-  try {
-    const result = await mongodb
-      .getDb()
-      .collection("contacts")
-      .find()
-      .toArray();
+const getAll = async (req, res) => {
+  const result = await mongodb.getDb().collection("contacts").find();
+  result.toArray().then((lists) => {
     res.setHeader("Content-Type", "application/json");
-    res.status(200).json(result);
-  } catch (err) {
-    next(err);
+    res.status(200).json(lists);
+  });
+};
+
+const getSingle = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .collection("contacts")
+    .find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(lists[0]);
+  });
+};
+
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDb()
+    .collection("contacts")
+    .insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact."
+      );
   }
 };
 
-const getSingle = async (req, res, next) => {
-  try {
-    if (!ObjectId.isValid(req.params.id)) {
-      return res.status(400).json({ message: "Invalid contact ID format" });
-    }
-
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDb()
-      .collection("contacts")
-      .findOne({ _id: userId });
-
-    if (!result) {
-      return res.status(404).json({ message: "Contact not found" });
-    }
-
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(result);
-  } catch (err) {
-    console.error("Error in getSingle:", err);
-    next(err);
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+  const response = await mongodb
+    .getDb()
+    .collection("contacts")
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while updating the contact."
+      );
   }
 };
 
-module.exports = { getAll, getSingle };
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDb()
+    .collection("contacts")
+    .deleteOne({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while deleting the contact."
+      );
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,23 @@
-require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
-const routes = require("./routes");
-const mongoose = require("mongoose");
-const cors = require("cors");
+
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.port || 8080;
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Acess-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/", require("./routes"));
 
 mongodb.initDb((err) => {
   if (err) {
-    console.error("failed to initialize DB", err);
-    process.exit(1);
+    console.log(err);
   } else {
-    console.log("DB initialized, starting server...");
-    app.use(express.json());
-    app.use("/contacts", routes);
-
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
   }
 });
-
-app.use(cors());
-app.use("/", require("./routes"));
-
-console.log(process.env.MONGODB_URI);
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
